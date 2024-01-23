4f7e401a29971b9dc333e7a1a94e536d6e5fbc51
aashu-akr <ashukr1901@gmail.com>
2024-01-23 18:01:55 +0530

    committed

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -18,11 +18,12 @@ const RestaurantMenu = () => {
 
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
+      <h1>Name of the Restaurant: </h1>
+      <h2>Menu: </h2>
       <ul>
         <li>Briyani</li>
         <li>Burgers</li>
+        <li>Fires</li>
         <li>Diet Coke</li>
       </ul>
     </div>
