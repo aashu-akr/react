e7ccc079c7529a4ac35943b370fe0bef5cc4da6f
aashu-akr <ashukr1901@gmail.com>
2024-02-07 18:19:41 +0530

    committed

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,7 +1,25 @@
 const Contact = () => {
   return (
     <div>
-      <h1>This is Contact Us page🚀</h1>
+      <h1 className="font-bold text-3xl m-5 p-5 text-center">
+        This is Contact Us page🚀
+      </h1>
+
+      <form className="flex text justify-center">
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="message"
+        />
+        <button className="border border-black rounded-md p-2 m-2">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
